74a8822c8a6e23ad725cc09f115484f12a05e349
Dmitry Klopov <strvngebeats@gmail.com>
2021-09-13 00:04:25 +0300

    добавил принты для логирования входящих данных на случай краша

--- main.py
+++ main.py
@@ -25,11 +25,13 @@ if __name__ == "__main__":
 
         if receiver_name in pro_users_name:
             bot.send_message(receiver_id, 'Собираю данные, результаты будут отправлены на почту указанную при оплате')
+            print(users.text)
             get_email_list_parsing(users=users.text)
             message_to_send = messages_adapter(raw_users_list=raw_users_list, raw_emails_list=raw_emails_list, command="email", subscription_type='pro')
             send_result_on_gmail(message=message_to_send, receiver_email=pro_users_email[receiver_name])
             bot.send_message(receiver_id, 'Результаты отправлены на почту {}'.format(pro_users_email[receiver_name]))
         else:
+            print(users.text)
             get_email_list_parsing(users=users.text)
             message_to_send = messages_adapter(raw_users_list=raw_users_list, raw_emails_list=raw_emails_list, command="email", subscription_type='free')
             send_result_on_telegram(message=message_to_send, receiver_id=receiver_id)
