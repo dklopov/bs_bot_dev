91f19fd8cdb8d1e02214dd95711e479e8bcd78d5
Dmitry Klopov <strvngebeats@gmail.com>
2021-09-12 20:42:07 +0300

    добавил проверку типа подиски пользователя.
    в зависимости от типа подписки добавил отправку результатов либо в телеграм, либо в почту

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from functions.messages_adapter import messages_adapter
 from functions.send_result import send_result_on_gmail
 from functions.send_result import send_result_on_telegram
 from classes.classes import Bot
+from users.pro_users import pro_users_name
+from users.pro_users import pro_users_email
 
 bot = telebot.TeleBot(Bot().get_token())
 
@@ -19,9 +21,18 @@ if __name__ == "__main__":
 
     def email_result(users):
         receiver_id = users.from_user.id
-        get_email_list_parsing(users=users.text)
-        message_to_send = messages_adapter(raw_users_list=raw_users_list, raw_emails_list=raw_emails_list, command="email", subscription_type='free')
-        send_result_on_telegram(message=message_to_send, receiver_id=receiver_id)
+        receiver_name = users.from_user.username
+
+        if receiver_name in pro_users_name:
+            get_email_list_parsing(users=users.text)
+            message_to_send = messages_adapter(raw_users_list=raw_users_list, raw_emails_list=raw_emails_list, command="email", subscription_type='pro')
+            send_result_on_gmail(message=message_to_send, receiver_email=pro_users_email[receiver_name])
+            bot.send_message(receiver_id, 'Результаты отправлены на почту {}'.format(pro_users_email[receiver_name]))
+        else:
+            get_email_list_parsing(users=users.text)
+            message_to_send = messages_adapter(raw_users_list=raw_users_list, raw_emails_list=raw_emails_list, command="email", subscription_type='free')
+            send_result_on_telegram(message=message_to_send, receiver_id=receiver_id)
+
         raw_users_list.clear()
         raw_emails_list.clear()
 
