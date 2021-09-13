85f85b36fab4a2f665a63aaecd53b3c87128ccfd
Dmitry Klopov <strvngebeats@gmail.com>
2021-09-14 01:02:45 +0300

    добавил проверку типа переменной users

--- functions/email_parsing.py
+++ functions/email_parsing.py
@@ -1,19 +1,36 @@
 from functions.connect_beatstars_com import connect_beatstars_com
 
 
-def get_email_list_parsing(users):
-    accounts_for_parsing = users.replace(" ", "").replace(".", ",").split(",")
-    n = 0
-    while n < len(accounts_for_parsing):
-        url_from_request = "/musician?permalink=" + accounts_for_parsing[n] + "&fields=profile,stats,email"
-        data_json = connect_beatstars_com(url_from_request=url_from_request)
-        try:
-            user_email = data_json['response']['data']['email']
-            raw_users_list.append(accounts_for_parsing[n])
-            raw_emails_list.append(user_email)
-            n += 1
-        except KeyError:
-            n += 1
+def get_email_list_parsing(users: str):
+    if type(users) == str:
+        accounts_for_parsing = users.replace(" ", "").replace(".", ",").split(",")
+        print(accounts_for_parsing)
+        n = 0
+        while n < len(accounts_for_parsing):
+            url_from_request = "/musician?permalink=" + accounts_for_parsing[n] + "&fields=profile,stats,email"
+            data_json = connect_beatstars_com(url_from_request=url_from_request)
+            try:
+                user_email = data_json['response']['data']['email']
+                raw_users_list.append(accounts_for_parsing[n])
+                raw_emails_list.append(user_email)
+                n += 1
+            except KeyError:
+                n += 1
+    elif type(users) == list:
+        accounts_for_parsing = users
+        print(accounts_for_parsing)
+        n = 0
+        while n < len(accounts_for_parsing):
+            url_from_request = "/musician?permalink=" + accounts_for_parsing[n] + "&fields=profile,stats,email"
+            data_json = connect_beatstars_com(url_from_request=url_from_request)
+            try:
+                user_email = data_json['response']['data']['email']
+                raw_users_list.append(accounts_for_parsing[n])
+                raw_emails_list.append(user_email)
+                n += 1
+            except KeyError:
+                n += 1
+
     return raw_users_list, raw_emails_list
 
 
