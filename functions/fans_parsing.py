77c5e029bea460ace136b838c8b9f65c17b54d45
Dmitry Klopov <strvngebeats@gmail.com>
2021-09-14 01:02:10 +0300

    добавил функцию

--- functions/fans_parsing.py
+++ functions/fans_parsing.py
@@ -4,6 +4,7 @@ from functions.connect_beatstars_com import connect_beatstars_com
 def get_fans_list_for_parsing(track_url: str):
     track_id = track_url.split("-")[1]
     url_from_request = ('https://main.v2.beatstars.com/fans/track/?id=' + str(track_id) + '&list_limit=1000')
+    print(url_from_request)
     data_json = connect_beatstars_com(url_from_request=url_from_request)
     data_list = data_json['response']['data']['list']
     n = 0
